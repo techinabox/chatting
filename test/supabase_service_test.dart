af0540173bbacf4ebf782f403e28b82ec110baec
raychr12 <ray.techinabox@gmail.com>
2026-07-01 16:54:09 -0500

    test: fix broken tests after initialization and app scaffold changes

--- test/supabase_service_test.dart
+++ test/supabase_service_test.dart
@@ -2,9 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ephemeral_chat/services/supabase_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
   test('SupabaseService initializes successfully', () async {
+    SharedPreferences.setMockInitialValues({});
     dotenv.testLoad(fileInput: '''
 SUPABASE_URL=https://mock.supabase.co
 SUPABASE_ANON_KEY=mock_anon_key
