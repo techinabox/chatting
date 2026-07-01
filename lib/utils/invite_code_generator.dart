f7bb3976038facc67a19ae8e72f0caeac1bc2187
raychr12 <ray.techinabox@gmail.com>
2026-07-01 16:39:55 -0500

    fix: enforce min length and rename to generateSecureCode

--- lib/utils/invite_code_generator.dart
+++ lib/utils/invite_code_generator.dart
@@ -1,7 +1,10 @@
 import 'dart:math';
 
 class InviteCodeGenerator {
-  static String generate(int length) {
+  static String generateSecureCode(int length) {
+    if (length < 20) {
+      throw ArgumentError('Length must be at least 20');
+    }
     const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     final random = Random.secure();
     return String.fromCharCodes(Iterable.generate(
--- test/invite_code_generator_test.dart
+++ test/invite_code_generator_test.dart
@@ -3,8 +3,14 @@ import 'package:ephemeral_chat/utils/invite_code_generator.dart';
 
 void main() {
   test('Generates 20+ char alphanumeric code', () {
-    final code = InviteCodeGenerator.generate(20);
+    final code = InviteCodeGenerator.generateSecureCode(20);
     expect(code.length, greaterThanOrEqualTo(20));
     expect(RegExp(r'^[a-zA-Z0-9]+$').hasMatch(code), isTrue);
+  });
+
+  test('Throws ArgumentError if length is less than 20', () {
+    expect(() => InviteCodeGenerator.generateSecureCode(19), throwsArgumentError);
+    expect(() => InviteCodeGenerator.generateSecureCode(0), throwsArgumentError);
+    expect(() => InviteCodeGenerator.generateSecureCode(-1), throwsArgumentError);
   });
 }
